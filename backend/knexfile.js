9940ed99b2abd490ae78a9ae4d04c090ca60453d
Arthurgallina1 <arthur.gallina@gmail.com>
2020-09-30 16:15:08 -0300

    add dotenv

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
     development: {
         client: "pg",
         connection: {
-            user: "postgres",
-            password: "postgres",
-            database: "ihero",
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
         },
         migrations: {
             // tableName: 'knex_migrations',
--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -4,43 +4,50 @@ const jwt = require("jsonwebtoken");
 const authConfig = require("../config/keys");
 
 module.exports = {
-  /**
+    /**
     @route POST user/auth
     @desc Auth user login
     @access Public
     */
-  async auth(req, res) {
-    const { username, password } = req.body;
-    try {
-      const user = await connection("users")
-        .where("username", username)
-        .select("id", "name", "email", "username", "password_hash", "createdAt")
-        .first();
-      if (!user) {
-        return res.status(400).json({
-          msg: "User not found!",
-          success: false,
-        });
-      }
+    async auth(req, res) {
+        try {
+            const { username, password } = req.body;
+            const user = await connection("users")
+                .where("username", username)
+                .select(
+                    "id",
+                    "name",
+                    "email",
+                    "username",
+                    "password_hash",
+                    "createdAt"
+                )
+                .first();
+            if (!user) {
+                return res.status(400).json({
+                    msg: "User not found!",
+                    success: false,
+                });
+            }
 
-      if (!(await bcrypt.compare(password, user.password_hash))) {
-        return res.status(400).json({
-          msg: "Invalid password!",
-          success: false,
-        });
-      }
+            if (!(await bcrypt.compare(password, user.password_hash))) {
+                return res.status(400).json({
+                    msg: "Invalid password!",
+                    success: false,
+                });
+            }
 
-      user.password_hash = undefined;
+            user.password_hash = undefined;
 
-      return res.json({
-        user,
-        token: jwt.sign({ id: user.id }, authConfig.secret, {
-          expiresIn: "7d",
-        }),
-      });
-    } catch (err) {
-      console.log(err);
-      return res.json(err).status(400);
-    }
-  },
+            return res.json({
+                user,
+                token: jwt.sign({ id: user.id }, authConfig.secret, {
+                    expiresIn: "7d",
+                }),
+            });
+        } catch (err) {
+            console.log(err);
+            return res.json(err).status(400);
+        }
+    },
 };
